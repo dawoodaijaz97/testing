58fce66d9f1c315e39b9a68f2a7eb5f51df5c935
Dawood <dawood.aijaz97@gmail.com>
2020-04-17 19:40:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const kneecap = require('kneecap');
+
+
+
+let handleRequest = function(request) {
+    return Promise.all([request.getRequestHeaders(), request.getRawBody()])
+        .then(results => {
+            const [requestHeaders, requestBody] = results;
+            console.log(requestHeaders);
+            console.log(requestBody);
+            return;
+        });
+}
+
+const kc = kneecap();
+kc.listen(8008).then(() => {
+    console.log('listening');
+});
+
+kc.requestHandler('/request', {
+    previewBytes: 128,
+    transfer: {
+        complete: ['*'],
+        ignore: [],
+        preview: ['*'],
+    }
+}, handleRequest);
